a4e882b86f801832f9dd6cc904ab997fd9c81246
Zachary D. Rowitsch <zrowitsch@riverbed.com>
2016-12-05 15:54:06 -0500

    Uncomment puppet-library install/update

--- provision/scripts/main.sh
+++ provision/scripts/main.sh
@@ -10,9 +10,9 @@ apt-get --yes --force-yes install puppet rubygems-integration unzip
  
 if [ "$(gem search -i librarian-puppet)" = "false" ]; then
   gem install librarian-puppet
-  #cd $PUPPET_DIR && librarian-puppet install
+  cd $PUPPET_DIR && librarian-puppet install
 else
-  #cd $PUPPET_DIR && librarian-puppet update
+  cd $PUPPET_DIR && librarian-puppet update
   echo Ok
 fi
 
